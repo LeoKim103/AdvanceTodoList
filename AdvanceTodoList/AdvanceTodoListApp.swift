ca2132e004a7804871e296601113f2983c55fe32
Leo Kim <leokim1032209@gmail.com>
2021-10-11 14:27:23 +0700

    starting project

---
+++ AdvanceTodoList/AdvanceTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceTodoListApp.swift
+//  AdvanceTodoList
+//
+//  Created by Leo Kim on 11/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
